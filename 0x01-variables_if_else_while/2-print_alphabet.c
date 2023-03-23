c20e16aaff7d55a0dd5a5f64495da844c8ca59c9
Muhmdfathi <mf2535185@gmail.com>
2023-03-23 16:15:31 -0700

    My first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main - Program to print alphabet letters in all cases followed by new line
+ *main - Program to print alphabet letter followed by new line
  *
  *Return: return 0
-*/
+ */
 int main(void)
 {
 int letter;
@@ -12,15 +12,6 @@ for (letter = 'a'; letter <= 'z'; letter++)
 {
 letter = tolower(letter);
 putchar(letter);
-if (letter == 'z')
-{
-letter = 'A';
-for (; letter <= 'Z'; letter++)
-{
-putchar(letter);
-}
-break;
-}
 }
 putchar('\n');
 return (0);
